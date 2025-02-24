cfe66e73a969a789d04c4bcfa87ff27369a1809a
Sara Han <sarahan774@gmail.com>
2025-02-24 23:26:44 +0900

    [FR-07] 테스트코드 수정

--- src/test/kotlin/usecase/CheckLowStockUseCaseTest.kt
+++ src/test/kotlin/usecase/CheckLowStockUseCaseTest.kt
@@ -4,6 +4,7 @@ import com.august.domain.model.Wine
 import com.august.service.alert.AlertService
 import com.august.service.inventory.InventoryRepository
 import com.august.usecase.CheckLowStockUseCase
+import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
@@ -29,9 +30,8 @@ class CheckLowStockUseCaseTest {
 
     @Test
     fun `given lowStockThreshold 보다 작은 수량의 와인, when execute, then sendAlert 함수가 호출된다`() {
-        every { inventoryRepository.getById("1") } returns Wine(
+        every { inventoryRepository.getById("1") } returns stubWine(
             id = "1", quantity = LOW_STOCK_THRESHOLD - 1,
-            wineryName = "", countryCode = "", vintage = 0, price = 0,
         )
         sut.execute("1")
         verify(exactly = 1) { alertService.sendAlert(any()) }
@@ -39,9 +39,8 @@ class CheckLowStockUseCaseTest {
 
     @Test
     fun `given lowStockThreshold 와 같은 수량의 와인, when execute, then sendAlert 함수 호출된다`() {
-        every { inventoryRepository.getById("1") } returns Wine(
+        every { inventoryRepository.getById("1") } returns stubWine(
             id = "1", quantity = LOW_STOCK_THRESHOLD,
-            wineryName = "", countryCode = "", vintage = 0, price = 0,
         )
         sut.execute("1")
         verify(exactly = 1) { alertService.sendAlert(any()) }
@@ -49,23 +48,30 @@ class CheckLowStockUseCaseTest {
 
     @Test
     fun `given lowStockThreshold 보다 큰 수량의 와인, when execute, then sendAlert 함수 호출 되지 않는다`() {
-        every { inventoryRepository.getById("1") } returns Wine(
+        every { inventoryRepository.getById("1") } returns stubWine(
             id = "1", quantity = LOW_STOCK_THRESHOLD + 1,
-            wineryName = "", countryCode = "", vintage = 0, price = 0,
         )
         sut.execute("1")
-        verify (exactly = 0) { alertService.sendAlert(any()) }
+        verify(exactly = 0) { alertService.sendAlert(any()) }
     }
 
     @Test
     fun `given 주어진 windId, when getById 실패, then sendAlert 함수 호출 되지 않는다`() {
         every { inventoryRepository.getById("1") } returns null
         sut.execute("1")
-        verify (exactly = 0) { alertService.sendAlert(any()) }
+        verify(exactly = 0) { alertService.sendAlert(any()) }
+        // confirmVerified(alertService) // ensure no other calls were made --> 이게 언제 필요할까?
+    }
+
+    private fun stubWine(id: String, quantity: Int): Wine {
+        return Wine(
+            id = id,
+            quantity = quantity,
+            wineryName = "", countryCode = "", vintage = 0, price = 0, // ignore other values
+        )
     }
 
     companion object {
         private const val LOW_STOCK_THRESHOLD = 1
     }
-
 }
