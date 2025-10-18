20d29ce10b83a7f9523908b709d9a2b4af4b5df5
Sara Han <sarahan774@gmail.com>
2025-10-18 23:45:15 +0900

    feat: frontend 정적 파일들 추가
    - index.html: 메인 HTML 템플릿
    - public/icon.svg: 앱 아이콘
    - public/manifest.json: PWA 매니페스트
    - public/sw.js: 서비스 워커 (PWA 지원)

---
+++ frontend/public/sw.js
@@ -0,0 +1,50 @@
+// 서비스 워커 - PWA 기능을 위한 오프라인 지원
+const CACHE_NAME = 'wine-inventory-v1';
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/manifest.json'
+];
+
+// 설치 이벤트
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('캐시 열기');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 페치 이벤트
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // 캐시에서 찾으면 반환
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// 활성화 이벤트
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('이전 캐시 삭제:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
